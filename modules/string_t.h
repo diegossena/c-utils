477134e0f668cc47d3c5cd59d9686c14628d0c4d
Diego <diego.sena@grupojrb.com.br>
2022-10-29 19:31:52 -0300

    29/10 + string_t&git push

--- modules/string_t.h
+++ modules/string_t.h
@@ -6,31 +6,73 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct
+// string type
+
+typedef struct string_t
 {
-  const char* buffer;
-  size_t length;
+	const char* ptr;
+	size_t length;
 } _string_t;
 
-void _str_free(const _string_t* str) {
-  printf("str_free");
-  if (str->buffer)
-    free((void*)str->buffer);
+#define string_null ((string_t){ 0, 0 })
+#define string_empty ((string_t){ "", 0 })
+
+// string properties //
+
+size_t string_length(const _string_t str) { return str.length; }
+const char* string_ptr(const _string_t str) { return str.ptr; }
+
+// string memory control //
+
+void _string_free(const _string_t* str) {
+	printf("str_free");
+	if (str->length)
+		free((void*)str->ptr);
 }
 
-#define string_t	_string_t __attribute__((cleanup(_str_free)))
+#define string_t	_string_t __attribute__((cleanup(_string_free)))
 
-#define string_empty ((string_t){ 0, 0 })
 
-#define str_cat_range(dest, src, count)	\
+// concatenate strings
+int _string_cat_range(_string_t* const dest, const _string_t* src, size_t count) {
+	printf("_string_cat_range");
+	// if (!src)
+	// {
+	// 	str_clear(dest);
+	// 	return 0;
+	// }
+
+	// // calculate total length
+	// const size_t num = total_length(src, count);
+
+	// if (num == 0)
+	// {
+	// 	str_clear(dest);
+	// 	return 0;
+	// }
+
+	// // allocate
+	// char* const buff = ALLOC(num + 1);
+
+	// // copy bytes
+	// char* p = buff;
+
+	// for (; count > 0; --count)
+	// 	p = append_str(p, *src++);
+
+	// // null-terminate and assign
+	// *p = 0;
+	// str_assign(dest, str_acquire_chars(buff, num));
+	return 0;
+}
+
+#define string_cat_range(dest, src, count)	\
 	_Generic((dest),	\
-		str*:	_str_cat_range,	\
-		int:	_str_cat_range_to_fd,	\
-		FILE*:	_str_cat_range_to_stream	\
+		str*:	_string_cat_range	\
 	)((dest), (src), (count))
 
-#define str_cat(dest, ...)	\
+#define string_cat(dest, ...)	\
 ({	\
 	const str args[] = { __VA_ARGS__ };	\
-	str_cat_range((dest), args, sizeof(args)/sizeof(args[0]));	\
+	string_cat_range((dest), args, sizeof(args)/sizeof(args[0]));	\
 })
--- src/main.c
+++ src/main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include "string_t.h"
 
-
 int main(int argc, char** argv) {
+
   string_t test = string_empty;
 
-  printf("test: '%s'\n", test);
+  printf("test: '%s'\n", string_ptr(test));
 
   return 0;
 }
