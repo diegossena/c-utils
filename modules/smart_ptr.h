d24d3e5543e25438f1b5a920b6e6148395481cc0
Diego <diego.sena@grupojrb.com.br>
2022-10-29 23:18:38 -0300

    29/10 + Window.h&git push

--- modules/smart_ptr.h
+++ modules/smart_ptr.h
@@ -3,13 +3,7 @@
 #include <malloc.h>
 
 void free_stack(void* ptr) {
-  // union {
-  //   void** real_ptr;
-  //   void* ptr;
-  // } conv;
-  // conv.ptr = ptr;
   free(*(void**)ptr);
-  // *conv.real_ptr = 0;
 }
 
 #define smart __attribute__ ((cleanup(free_stack)))
