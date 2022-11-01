faecb08a39718b7d7d27c7b7cdeaa342cdd10a1b
Diego <diego.sena@grupojrb.com.br>
2022-11-01 09:19:24 -0300

    01/11

--- src/main.c
+++ src/main.c
@@ -2,8 +2,28 @@
 #include "String_t.h"
 #include "Window.h"
 
-void windowCallback(Window* window) {
+void window_Callback(Window* window) {
   switch (window_event_get(window)) {
+
+  case WINDOW_QUIT:
+    printf("WINDOW_QUIT\n");
+    break;
+
+  case WINDOW_UNDEFINED:
+    printf("WINDOW_UNDEFINED\n");
+    break;
+
+  case WINDOW_FOCUS:
+    printf("WINDOW_FOCUS\n");
+    break;
+
+  case WINDOW_BLUR:
+    printf("WINDOW_BLUR\n");
+    break;
+
+  case WINDOW_RESIZE:
+    printf("WINDOW_RESIZE\n");
+    break;
 
   case WINDOW_MOUSE_MOVE: {
     int xPos = window_mouse_get_x(window);
@@ -16,7 +36,7 @@ void windowCallback(Window* window) {
 
 int main() {
 
-  window_new_centered(windowCallback, "test1", 800, 600, WINDOW_RESIZABLE);
+  window_new_centered(window_Callback, "test1", 800, 600, WINDOW_RESIZABLE);
 
   windows_run();
 
