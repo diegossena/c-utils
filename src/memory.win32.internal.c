25bc3fe53c487f7371c22bcbd33152fdcd8a3bce
Diego Sena <diego.souza.sena10@gmail.com>
2024-03-04 22:32:10 -0300

    *

--- src/memory.win32.internal.c
+++ src/memory.win32.internal.c
@@ -14,26 +14,32 @@ i64 memory_counter = 0;
 
 void* memory_alloc(u64 size) {
   void* block = HeapAlloc(GetProcessHeap(), 0, size);
-  if (!block) {
+  if (block) {
+    ++memory_counter;
+  } else {
     error("HeapAlloc", ERR_NOT_ENOUGH_MEMORY);
   }
   return block;
 }
 void* memory_alloc0(u64 size) {
   void* block = HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, size);
-  if (!block) {
+  if (block) {
+    ++memory_counter;
+  } else {
     error("HeapAlloc", ERR_NOT_ENOUGH_MEMORY);
   }
   return block;
 }
 void memory_free(void* this) {
   HeapFree(GetProcessHeap(), 0, this);
+  --memory_counter;
 }
 void* memory_realloc(void* this, u64 size) {
   if (this) {
     this = HeapReAlloc(GetProcessHeap(), 0, this, size);
   } else {
     this = HeapAlloc(GetProcessHeap(), 0, size);
+    ++memory_counter;
   }
   if (!this) {
     error("HeapReAlloc", ERR_NOT_ENOUGH_MEMORY);
@@ -45,6 +51,7 @@ void* memory_realloc0(void* this, u64 size) {
     this = HeapReAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, this, size);
   } else {
     this = HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, size);
+    ++memory_counter;
   }
   if (!this) {
     error("HeapReAlloc", ERR_NOT_ENOUGH_MEMORY);
