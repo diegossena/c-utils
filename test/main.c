8a54c574f9642b2b42fc9427814bf9007cb6e20a
Diego Sena <diego.souza.sena10@gmail.com>
2024-03-06 19:53:32 -0300

    *

--- test/main.c
+++ test/main.c
@@ -81,8 +81,8 @@ i32 main() {
   // map_test();
   // net_tcp_test();
   // net_tcp_server_test();
-  // window_test();
-  path_test();
+  // path_test();
+  window_test();
 
   app_run();
   console_log_cstr(CONSOLE_FORE_GREEN "SUCCESS" CONSOLE_FORE_GREEN CONSOLE_RESET);
