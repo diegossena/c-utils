13cb6accf126ec252e61a458d5e277a806cf5d74
Diego Sena <diego.souza.sena10@gmail.com>
2024-03-11 00:19:57 -0300

    *

--- include/sdk/math.h
+++ include/sdk/math.h
@@ -7,16 +7,6 @@
 #define math_max(a, b) (a < b ? b : a)
 #define math_clamp(value, min, max) (math_min(math_max(value, min), max))
 #define math_abs(value) (value < 0 ? -(value) : value)
-/*
-offset_y=-9.5
-visible_tiles_y=19
-context.level_height=16
-result = -3
-
-math_clamp(offset_y, 0.f, (f32)context.level_height - visible_tiles_y);
-
-0 < a ? 0 : a
-*/
 
 u64 math_next2pow(u64 number);
 u64 math_hash_jenkins(const byte* data, u64 size);
