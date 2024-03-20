1db134eae6d11638ed87a7cee8df5d5cac8c8294
Diego Sena <diego.souza.sena10@gmail.com>
2024-03-19 22:34:38 -0300

    *

--- packages/rpg/game/routines/moveto.h
+++ packages/rpg/game/routines/moveto.h
@@ -9,8 +9,7 @@ typedef struct moveto_t {
   // props
   game_t* game;
   vector2d_t* position;
-  vector2d_t start;
-  vector2d_t target;
+  vector2d_t distance;
   f32 duration;
   f32 timer;
 } moveto_t;
@@ -24,15 +23,10 @@ void routine_moveto_destroy(moveto_t* this) {
 }
 void routine_moveto_update(moveto_t* this, f32 elapsed_time) {
   this->timer += elapsed_time;
-  f32 tick = this->timer / this->duration;
-  if (tick > 1.f) tick = 1.f;
-
-  this->position->x = this->start.x + tick * (this->target.x - this->start.x);
-  this->position->y = this->start.y + tick * (this->target.y - this->start.y);
-
+  f32 tick = elapsed_time / this->duration;
+  this->position->x += tick * this->distance.x;
+  this->position->y += tick * this->distance.y;
   if (this->timer >= this->duration) {
-    // this->position->x = this->target.x;
-    // this->position->y = this->target.y;
     routine_moveto_destroy(this);
   }
 }
@@ -41,9 +35,10 @@ void routine_moveto(game_t* game, vector2d_t* position, vector2d_t target, f32 d
   moveto_t* this = memory_alloc(sizeof(moveto_t));
   this->game = game;
   this->position = position;
-  this->start = *position;
-  this->target = target;
-  this->duration = duration;
+  this->distance.x = target.x - position->x;
+  this->distance.y = target.y - position->y;
+  this->duration = math_max(duration, .001f);
+  this->timer = 0.f;
   // register
   this->onupdate = (update_listener_t) {
     .callback = (onupdate_callback_t)routine_moveto_update,
