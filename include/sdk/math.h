6c674e21668950e1d7419ab2ee9d229c9fa052ac
Diego Sena <diego.souza.sena10@gmail.com>
2024-03-21 12:57:35 -0300

    *

--- include/sdk/math.h
+++ include/sdk/math.h
@@ -6,7 +6,7 @@
 #define math_min(a, b) (b < a ? b : a)
 #define math_max(a, b) (a < b ? b : a)
 #define math_clamp(value, min, max) (math_min(math_max(value, min), max))
-#define math_abs(value) (value < 0 ? (-value) : value)
+#define math_abs(value) (value < 0 ? -(value) : value)
 
 u64 math_next2pow(u64 number);
 u64 math_hash_jenkins(const byte* data, u64 size);
