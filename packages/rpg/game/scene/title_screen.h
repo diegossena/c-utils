a234b680c22931cd16d239a29a1a1eeb3659ce77
Diego Sena <diego.souza.sena10@gmail.com>
2024-03-21 15:11:37 -0300

    *

--- packages/rpg/game/scene/title_screen.h
+++ packages/rpg/game/scene/title_screen.h
@@ -20,13 +20,11 @@ typedef struct title_screen_t {
   gfx_stroke_t stroke_solid;
   gfx_style_t title_style, play_style;
  // elements
-  gfx_rect_t container;
   bool container_clicking;
   gfx_text_t title, press_space, to_play;
 } title_screen_t;
 
 void titlescreen_onupdate(title_screen_t* this) {
-  gfx_rect_draw(&this->container);
   gfx_text_draw(&this->title);
   gfx_text_draw(&this->press_space);
   gfx_text_draw(&this->to_play);
@@ -61,16 +59,6 @@ void titlescreen_load(game_t* game) {
   gfx_color_new(&this->color_black, window, COLOR_BLACK);
   gfx_color_new(&this->color_green, window, COLOR_GREEN);
   // elements
-  this->container = (gfx_rect_t) {
-    .window = window,
-    .rect = { 230.f },
-    .color = &this->color_black,
-    .stroke = &this->stroke_solid,
-    .border_width = 1.f
-  };
-  rect_set_width(&this->container.rect, 290.f);
-  rect_set_height(&this->container.rect, 70.f);
-
   gfx_style_new(&this->title_style, (gfx_style_props_t) {
     .window = window,
       .family = L"TLOZ Minish Cap/A Link to the Past/Four Sword",
@@ -81,7 +69,7 @@ void titlescreen_load(game_t* game) {
   this->title = (gfx_text_t) {
     .window = window,
     gfx_text_cwstr(L"DreamShifters"),
-    .position = this->container.rect.left_top,
+    .position = { 250.f, 10.f },
     .color = &this->color_black,
     .format = &this->title_style
   };
