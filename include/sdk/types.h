276b9a3f75a64c914aa33c39e84f72281bf7f2ff
Diego Sena <diego.souza.sena10@gmail.com>
2024-03-21 12:34:26 -0300

    *

--- include/sdk/types.h
+++ include/sdk/types.h
@@ -9,6 +9,18 @@
 #define true 1
 #define false 0
 
+#define MAX_I16 32767
+#define MIN_I16 -32768
+#define MAX_U16 0xffffU
+#define MAX_I32 2147483647L
+#define MIN_I32 (-MAX_I32 - 1)
+#define MAX_U32 0xffffffffUL
+#define MAX_U64 0xffffffffffffffffull
+#define MAX_I64 9223372036854775807ll
+#define MIN_I64 (-MAX_I64 - 1)
+#define MAX_F32 3.40282e+38F
+#define MIN_F32 -MAX_F32
+
 typedef _Bool bool;
 
 typedef signed char i8; // -128 ~ 127
@@ -25,42 +37,6 @@ typedef float f32; // -3.40282e+38 ~ 3.40282e+38
 typedef double f64; // -1.79769e+308 ~ 1.79769e+308
 
 typedef u8 byte;
-
 typedef u16 wchar_t;
 
 typedef void (*fn_any_t)();
-
-#define MAX_I16 32767
-#define MIN_I16 -32768
-#define MAX_U16 0xffffU
-#define MAX_I32 2147483647L
-#define MIN_I32 (-MAX_I32 - 1)
-#define MAX_U32 0xffffffffUL
-#define MAX_U64 0xffffffffffffffffull
-#define MAX_I64 9223372036854775807ll
-#define MIN_I64 (-MAX_I64 - 1)
-#define MAX_F32 3.40282e+38F
-#define MIN_F32 -MAX_F32
-
-// Inlining
-#if defined(__clang__) || defined(__gcc__)
-/** @brief Inline qualifier */
-#define sdk_inline __attribute__((always_inline)) inline
-
-/** @brief No-inline qualifier */
-#define sdk_no_inline __attribute__((noinline))
-#elif defined(_MSC_VER)
-
-/** @brief Inline qualifier */
-#define sdk_inline __forceinline
-
-/** @brief No-inline qualifier */
-#define sdk_no_inline __declspec(noinline)
-#else
-
-/** @brief Inline qualifier */
-#define sdk_inline static inline
-
-/** @brief No-inline qualifier */
-#define sdk_no_inline
-#endif
