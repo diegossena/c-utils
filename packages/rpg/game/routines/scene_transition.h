419a5f26fcd88e4902fdbb8c4369f74ea63b450f
Diego Sena <diego.souza.sena10@gmail.com>
2024-03-22 01:08:34 -0300

    *

--- packages/rpg/game/routines/scene_transition.h
+++ packages/rpg/game/routines/scene_transition.h
@@ -28,9 +28,9 @@ void scene_transition_destroy(scene_transition_t* this) {
   this->game->__in_transition = false;
 }
 void scene_transition_update(scene_transition_t* this) {
+  static const f32 duration = .8f;
   game_t* game = this->game;
   window_t* window = game->window;
-  static const f32 duration = .8f;
   this->timer += game->elapsed_time;
   f32 progress = this->timer / duration;
   if (progress > 1.f) {
