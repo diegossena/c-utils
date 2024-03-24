e31447ea8ccdb009a7d7b4309e16bd4d204effda
Diego Sena <diego.souza.sena10@gmail.com>
2024-03-24 13:47:51 -0300

    *

--- include/sdk/keyboard/inl.h
+++ include/sdk/keyboard/inl.h
@@ -6,7 +6,7 @@ static u8 __keyboard_state[32] = {};
 bool keyboard_pressed(key_code_t key) {
   u8 byte_index = key / 8;
   u8 bit_index = key % 8;
-  return (__keyboard_state[byte_index] >> bit_index) & 1;
+  return __keyboard_state[byte_index] & (1 << bit_index);
 }
 void __keyboard_press(key_code_t key) {
   u8 byte_index = key / 8;
