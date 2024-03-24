1502bd1fa9d92ac2e7ce42e2061fe9de38d5c9ab
Diego Sena <diego.souza.sena10@gmail.com>
2024-03-24 13:50:26 -0300

    *

--- packages/rpg/game/items/item.h
+++ packages/rpg/game/items/item.h
@@ -2,13 +2,13 @@
 
 #include <sdk/string.h>
 #include <sdk/window.h>
-#include <sdk/window/gfx/bitmap.h>
+#include <sdk/window/gfx/image.h>
 #include "../entities/entity.h"
 
 typedef struct item_t {
   queue_t queue;
   string_t name;
   string_t description;
-  image_t* sprite;
+  gfx_image_src_t* sprite;
   void (*onuse)(entity_t*);
 } item_t;
