becc5e703b79f863b96edbd7e1158ff4870bce02
Diego Sena <diego.souza.sena10@gmail.com>
2024-03-26 16:10:27 -0300

    *

--- include/rpg/components/player.inl.h
+++ include/rpg/components/player.inl.h
@@ -10,25 +10,27 @@ void player_onupdate(player_t* this) {
   if (this->walking_timer <= 0)
     return;
   static const f32 duration = .25f;
-  local_map_t* map = this->map;
-  game_t* game = map->game;
+  local_map_t* local_map = this->map;
+  game_t* game = local_map->game;
   window_t* window = game->window;
   this->walking_timer += window->elapsed_time;
   f32 progress = this->walking_timer / duration;
   if (progress > 1.f) {
     progress = 1.f;
-  } else if (this->state == this->start_state && this->walking_timer >= .075f) {
+  } else if (this->start_state == this->state && progress >= .075f) {
     this->state = (++this->state) % PLAYER_STATE_MAX;
   }
   this->x = this->start_x + progress * this->distance_x;
   this->y = this->start_y + progress * this->distance_y;
-  window_render_request(window);
+  local_map->camera.x = this->x + .5f;
+  local_map->camera.y = this->y + 1.f;
   if (progress >= 1.f) {
     this->distance_x = 0;
     this->distance_y = 0;
     this->walking_timer = 0;
     this->state = (++this->state) % PLAYER_STATE_MAX;
   }
+  window_render_request(window);
 }
 void player_draw(player_t* this) {
   static const f32 sprite_width = 15.f;
@@ -47,8 +49,6 @@ void player_draw(player_t* this) {
       (this->y - local_map->offset.y) * TILE_SIZE,
     },
   };
-  local_map->camera.x = this->x + .5f;
-  local_map->camera.y = this->y + 1.f;
   switch (this->direction) {
     case PLAYER_UP:
       player.src_position.y = 75;
@@ -89,23 +89,31 @@ void player_onkeypress(player_t* this) {
   this->start_x = this->x;
   this->start_y = this->y;
   this->start_state = this->state;
+  bool update = false;
   if (keyboard_pressed(KEY_UP)) {
     this->distance_y -= 1.f;
     this->direction = PLAYER_UP;
+    update = true;
   } else if (keyboard_pressed(KEY_DOWN)) {
     this->distance_y += 1.f;
     this->direction = PLAYER_DOWN;
+    update = true;
   }
   if (keyboard_pressed(KEY_RIGHT)) {
     this->distance_x += 1.f;
     this->direction = PLAYER_RIGHT;
+    update = true;
   } else if (keyboard_pressed(KEY_LEFT)) {
     this->distance_x -= 1.f;
     this->direction = PLAYER_LEFT;
+    update = true;
+  }
+  if (update) {
+    this->state = (++this->state) % PLAYER_STATE_MAX;
+    window_render_request(window);
   }
 }
 void player_onkeydown(player_t* this) {
-
 }
 void player_onkeyup(player_t* this) {
   // local_map_t* local_map = this->map;
