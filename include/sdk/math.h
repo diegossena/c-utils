ce003c4a709284e686b3c6fc896112a2f566dbdb
Diego Sena <diego.souza.sena10@gmail.com>
2024-03-27 16:26:54 -0300

    *

--- include/sdk/math.h
+++ include/sdk/math.h
@@ -6,7 +6,7 @@
 
 #define math_min(a, b) (b < a ? b : a)
 #define math_max(a, b) (a < b ? b : a)
-#define math_clamp(value, min, max) (math_min(math_max(value, min), max))
+#define math_clamp(value, min, max) math_min(math_max(value, min), max)
 #define math_abs(value) (value < 0 ? -(value) : value)
 
 #endif
