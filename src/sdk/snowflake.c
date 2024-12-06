46592440b34f7cfb06e3fcd4519e96e99e89d7be
Diego Sena <diego.souza.sena10@gmail.com>
2024-12-05 23:34:33 -0300

    *

--- src/sdk/snowflake.c
+++ src/sdk/snowflake.c
@@ -1,23 +1,25 @@
 #include <sdk/snowflake.h>
 
 #define SNOWFLAKE_MAX_SEQUENCE 1023ULL
+#define SNOWFLAKE_EPOCH 1577836800000ULL
+#define SNOWFLAKE_NODE 0ULL
 
-static u64 __snowflake_node = 0;
-static u64 __snowflake_epoch = 1577836800000;
-static u64 __snowflake_updated_at = 0;
-static u64 __snowflake_sequence = 0;
+u64 __snowflake_updated_at = 0;
+u64 __snowflake_sequence = 0;
 
 export u64 snowflake_uid() {
   u64
     current_time = date_now(),
-    uid_time = current_time - __snowflake_epoch;
+    uid_time = current_time - SNOWFLAKE_EPOCH;
   if (uid_time == __snowflake_updated_at) {
     ++__snowflake_sequence & SNOWFLAKE_MAX_SEQUENCE;
   } else {
     __snowflake_sequence = 0;
   }
   __snowflake_updated_at = uid_time;
-  return uid_time << 22 | __snowflake_node << 10 | __snowflake_sequence;
+  return uid_time << 22 | SNOWFLAKE_NODE << 10 | __snowflake_sequence;
 }
 
+#undef SNOWFLAKE_EPOCH
+#undef SNOWFLAKE_NODE
 #undef SNOWFLAKE_MAX_SEQUENCE
