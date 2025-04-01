0f74d7d0ded668dc95cd81cf75897eebe5eaa9a6
Diego Sena <diego.souza.sena10@gmail.com>
2025-04-01 11:17:40 -0300

    *

--- src/sdk/async.win32.c
+++ src/sdk/async.win32.c
@@ -2,17 +2,20 @@
 #ifdef PLATFORM_WINDOWS
 #include <windows.h>
 
+#define IOCP_SHUTDOWN ((u8)-1)
+
 thread_t* __async_thread = 0;
 HANDLE __iocp = 0;
+u64 __async_count = 0;
 
 export void async_startup() {
   assert(__async_thread == 0);
   __async_thread = thread_new((function_t)__async_worker, null);
-  __iocp = (HANDLE)true;
+  __iocp = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 0);
 }
 export void async_shutdown() {
   assert(__async_thread != 0);
-  __iocp = 0;
+  PostQueuedCompletionStatus(__iocp, 0, IOCP_SHUTDOWN, 0);
   thread_await(__async_thread);
 #if DEBUG
   __async_thread = 0;
@@ -20,16 +23,18 @@ export void async_shutdown() {
 }
 
 void __async_worker() {
-  // DWORD bytes_count;
-  // OVERLAPPED* overlapped;
-  // ULONG_PTR completion_key;
-  while (__iocp) {
-    Sleep(100);
-    // BOOL result = GetQueuedCompletionStatus(__iocp, &bytes_count, &completion_key, &overlapped, INFINITE);
-    // if (!result) {
-
-    // }
+  DWORD bytes_count;
+  OVERLAPPED* overlapped;
+  ULONG_PTR completion_key;
+  while (true) {
+    bool success = GetQueuedCompletionStatus(__iocp, &bytes_count, &completion_key, &overlapped, INFINITE);
+    if (!success)
+      continue;
+    if (completion_key == IOCP_SHUTDOWN)
+      break;
   }
+  CloseHandle(__iocp);
 }
 
+#undef IOCP_SHUTDOWN
 #endif
