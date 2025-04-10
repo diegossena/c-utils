319e6ac8238b490eafd9d9670ed5507dace1e8a7
Diego Sena <diego.souza.sena10@gmail.com>
2025-04-10 02:34:47 -0300

    *

--- tools/cso_reader.js
+++ tools/cso_reader.js
@@ -2,14 +2,17 @@ const fs = require('fs')
 
 const name = 'texture'
 
+const vs_cso_path = `assets/${name}.vs.cso`
+const ps_cso_path = `assets/${name}.ps.cso`
+
 const vs_cso = '{ '
-  + fs.readFileSync(`assets/${name}.vs.cso`, 'hex')
+  + fs.readFileSync(vs_cso_path, 'hex')
     .match(/.{2}/g)
     .map(hex => `0x${hex}`)
     .join(', ')
   + ' };'
 const ps_cso = '{ '
-  + fs.readFileSync(`assets/${name}.ps.cso`, 'hex')
+  + fs.readFileSync(ps_cso_path, 'hex')
     .match(/.{2}/g)
     .map(hex => `0x${hex}`)
     .join(', ')
@@ -17,3 +20,6 @@ const ps_cso = '{ '
 
 console.log('VS', vs_cso)
 console.log('PS', ps_cso)
+
+fs.rmSync(vs_cso_path)
+fs.rmSync(ps_cso_path)
