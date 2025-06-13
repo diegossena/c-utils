a875115f22c782b0cea85e2789eaecc17eee452c
Diego Sena <diego.souza.sena10@gmail.com>
2025-06-13 13:39:09 -0300

    *

--- test.js
+++ test.js
@@ -2,11 +2,35 @@ const NODE_TYPE_INPUT = 0
 const NODE_TYPE_HIDDEN = 1
 const NODE_TYPE_OUTPUT = 2
 
+
+/** 
+ * @typedef {object} NeatNode
+ * @property {number} id
+ * @property {number} type
+ * @property {number} bias
+ * @property {(x: number) => number} activation
+ * @property {(x: number[]) => number} aggregation
+ * @property {number} value
+ * @property {number} prev_value
+ */
+/**
+ * @typedef {object} Connection
+ * @property {NeatNode} in
+ * @property {NeatNode} out
+ * @property {number} weight
+ */
+
+/**
+ * @typedef {object} NodeLink
+ * @property {NeatNode} in
+ * @property {number} weight
+ */
+
 /**
  * @param {number[]} x 
  * @returns {number}
  */
-function sum_aggregation(x) {
+function aggregation_sum(x) {
   return x.reduce((acc, cur) => acc + cur, 0)
 }
 /**
@@ -16,117 +40,71 @@ function sum_aggregation(x) {
 function activation_relu(x) {
   return Math.max(0, x)
 }
-
+/**
+ * @param {number} x 
+ * @returns {number}
+ */
+function activation_identity(x) {
+  return x
+}
+/**
+ * @param {number} z 
+ * @returns {number}
+ */
+function activation_sigmoid(z) {
+  z = Math.max(-60, Math.min(60, 5 * z))
+  return 1.0 / (1.0 + Math.exp(-z))
+}
+/** @type {NeatNode[]} */
 const net_inputs = [
   {
-    id: 0,
     type: NODE_TYPE_INPUT,
     bias: 0,
-    activation: activation_relu,
-    aggregation: sum_aggregation,
-    value: Math.round(Math.random() * 100)
-  },
-  {
-    id: 1,
-    type: NODE_TYPE_INPUT,
-    bias: 0,
-    activation: activation_relu,
-    aggregation: sum_aggregation,
-    value: Math.round(Math.random() * 100)
+    activation: activation_identity,
+    aggregation: aggregation_sum,
   },
 ]
+/** @type {NeatNode[]} */
 const net_outputs = [
   {
-    id: 2,
     type: NODE_TYPE_OUTPUT,
     bias: 0,
-    activation: activation_relu,
-    aggregation: sum_aggregation,
-    value: 0
+    activation: x => x > 0.5 ? 1 : 0, // binary step,
+    aggregation: aggregation_sum,
   },
 ]
-
+/** @type {NeatNode[]} */
 const net_nodes = [
   ...net_inputs,
   ...net_outputs,
   {
-    id: 3,
     type: NODE_TYPE_HIDDEN,
-    activation: activation_relu,
-    aggregation: sum_aggregation,
     bias: 0,
-    value: 0
+    activation: activation_sigmoid,
+    aggregation: aggregation_sum,
   },
-  {
-    id: 4,
-    type: NODE_TYPE_HIDDEN,
-    activation: activation_relu,
-    aggregation: sum_aggregation,
-    bias: 0,
-    value: 0
-  },
-  {
-    id: 5,
-    type: NODE_TYPE_HIDDEN,
-    activation: activation_relu,
-    aggregation: sum_aggregation,
-    bias: 0,
-    value: 0
-  },
-  {
-    id: 6,
-    type: NODE_TYPE_HIDDEN,
-    activation: activation_relu,
-    aggregation: sum_aggregation,
-    bias: 0,
-    value: 0
-  },
-]
+].map((node, i) => {
+  node.id = i
+  node.value = 0
+  node.prev_value = 0
+  return node
+})
+/** @type {Connection[]} */
 const net_connections = [
   {
-    node_in: net_nodes[0],
-    node_out: net_nodes[3],
-    weight: .5,
+    in: net_nodes[0],
+    out: net_nodes[2],
+    weight: 1,
   },
   {
-    node_in: net_nodes[1],
-    node_out: net_nodes[3],
-    weight: .5,
+    in: net_nodes[2],
+    out: net_nodes[2],
+    weight: 1,
   },
   {
-    node_in: net_nodes[3],
-    node_out: net_nodes[4],
-    weight: .5,
-  },
-  {
-    node_in: net_nodes[4],
-    node_out: net_nodes[5],
-    weight: .5,
-  },
-  {
-    node_in: net_nodes[3],
-    node_out: net_nodes[5],
-    weight: .5,
-  },
-  {
-    node_in: net_nodes[5],
-    node_out: net_nodes[2],
-    weight: .5,
-  },
-  {
-    node_in: net_nodes[5],
-    node_out: net_nodes[6],
-    weight: .5,
-  },
-  {
-    node_in: net_nodes[5],
-    node_out: net_nodes[3],
-    weight: .5,
-  },
-  {
-    node_in: net_nodes[6],
-    node_out: net_nodes[2],
-    weight: .5,
+    in: net_nodes[2],
+    out: net_nodes[1],
+    weight: 1,
   },
 ]
 
@@ -136,11 +114,11 @@ if (true) {
   while (true) {
     const layer_nodes = net_connections
       .filter(connection => (
-        required_nodes.includes(connection.node_out)
-        && !required_nodes.includes(connection.node_in)
-        && !net_inputs.includes(connection.node_in)
+        required_nodes.includes(connection.out)
+        && !required_nodes.includes(connection.in)
+        && !net_inputs.includes(connection.in)
       ))
-      .map(connection => connection.node_in)
+      .map(connection => connection.in)
     if (!layer_nodes.length) {
       break
     }
@@ -151,10 +129,10 @@ if (true) {
   while (true) {
     const t = net_connections
       .filter(connection => (
-        processed_connections.includes(connection.node_out)
-        && !processed_connections.includes(connection.node_in)
+        processed_connections.includes(connection.out)
+        && !processed_connections.includes(connection.in)
       ))
-      .map(connection => connection.node_in)
+      .map(connection => connection.in)
     if (!t.length) {
       break
     }
@@ -167,39 +145,34 @@ if (true) {
   }
 }
 required_nodes.reverse()
-console.log('required_nodes', required_nodes)
-if (true) {
-  const required_connections = net_connections.filter(connection => (
-    required_nodes.includes(connection.node_in)
-    || required_nodes.includes(connection.node_out)
-  ))
-  for (const connection of required_connections) {
 
+// node_evals
+
+/** @type {Map<NeatNode, NodeLink[]>} */
+const node_links = new Map([])
+for (const connection of net_connections) {
+  if (!required_nodes.includes(connection.in) && !required_nodes.includes(connection.out))
+    continue
+  if (!node_links.has(connection.out)) {
+    node_links.set(connection.out, [])
   }
-  console.log('required_connections', required_connections)
-} else {
-  // node_evals
-  const node_inputs = new Map([])
-  for (const connection of net_connections) {
-    if (!required_nodes.includes(connection.node_in) || !required_nodes.includes(connection.node_out))
-      continue
-    if (node_inputs.has(connection.node_out)) {
-      node_inputs.get(connection.node_out).links.push({ in: connection.node_in, w: connection.weight })
-    } else {
-      node_inputs.set(connection.node_out, {
-        node: connection.node_out,
-        links: [{ in: connection.node_in, w: connection.weight }]
-      })
-    }
+  node_links.get(connection.out).push({ in: connection.in, weight: connection.weight })
+}
+const input_sequence = [
+  0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1
+]
+for (const input of input_sequence) {
+  // apply
+  net_inputs[0].value = input
+  // save_state
+  for (const node of net_nodes) {
+    node.prev_value = node.value
   }
-  const node_evals = [...node_inputs.values()]
-  console.log('node_evals', node_evals)
   // activate
-  for (const node_eval of node_evals) {
-    const { node, links } = node_eval
-    const node_inputs = links.map(link => link.in.value * link.w)
-    const s = node.aggregation(node_inputs)
-    node.value = node.activation(node.bias + node.value + s)
+  for (const [node, links] of node_links) {
+    const node_inputs = links.map(link => link.in.prev_value * link.weight)
+    const aggregation = node.aggregation(node_inputs)
+    node.value = node.activation(node.bias + aggregation)
   }
-  console.log('net_outputs', net_outputs)
+  console.log('output', input, net_outputs[0].value)
 }
