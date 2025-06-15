f28b1ffab6ab437b5d2b4149fed44a1a20665134
Diego Sena <diego.souza.sena10@gmail.com>
2025-06-15 03:47:53 -0300

    flappy_bird

--- src/flappy_bird/main.c
+++ src/flappy_bird/main.c
@@ -2,6 +2,7 @@
 #include <sdk/window.h>
 
 #define WINDOW_SCALING 3.8f
+#define OBJECTS_COUNT 5
 
 u16 window_width = 144 * WINDOW_SCALING;
 u16 window_height = 256 * WINDOW_SCALING;
@@ -22,18 +23,18 @@ typedef enum bird_wing_state_t {
   BIRD_WING_DOWN
 } bird_wing_state_t;
 
-const f32 bird_y0 = .4f;
-const f32 bird_animate_offset = .04f;
-f32 bird_animate_duration = .4f;
 f32 bird_animate_timer = 0;
 u8 bird_animate_state = BIRD_ANIMATE_DOWN;
 u8 bird_wing_state = BIRD_WING_MIDDLE;
+
+f32 ground_animate_timer = 0;
 
 void window_dblclick() {}
 void window_onmousemove() {}
 void window_onmousedown(i32 x, i32 y, mouse_btn_t button) {}
 void window_onmouseup(i32 x, i32 y, mouse_btn_t button) {}
 void window_onscroll(i32 delta) {}
+
 void window_onkeydown(key_t key) {}
 void window_onkeyup(key_t key) {}
 void window_onresize() {}
@@ -50,24 +51,47 @@ void window_onrender() {
   u1 = u0 + atlas_ndc_x * 144;
   v0 = atlas_ndc_y * 192;
   v1 = v0 + atlas_ndc_y * 64;
-  window_rect_fill(
-    -1, y1, 1, -1,
-    0.329411765f, 0.878431373f, 0.458823529f, 1
-  );
   window_rect_draw(x0, y0, x1, y1, u0, v0, u1, v1);
+  // ground_animate
+  const f32 ground_animate_duration = .2f;
+  ground_animate_timer += window_deltatime;
+  f32 ground_animate_progress = math_min(ground_animate_timer / ground_animate_duration, 1);
+  const f32 ground_animate_offset = window_ndc_x * 12 * WINDOW_SCALING * ground_animate_progress;
+  x0 = -1 - ground_animate_offset;
+  x1 = 1.f + (window_ndc_x * 16 * WINDOW_SCALING) - ground_animate_offset;
+  y0 = y1;
+  y1 = y0 - window_ndc_y * 16 * WINDOW_SCALING;
+  u0 = 0;
+  u1 = atlas_ndc_x * 160;
+  v0 = atlas_ndc_y * 160;
+  v1 = v0 + atlas_ndc_y * 16;
+  if (ground_animate_timer >= ground_animate_duration) {
+    ground_animate_timer = 0;
+  }
+  // ground_draw
+  window_rect_draw(x0, y0, x1, y1, u0, v0, u1, v1);
+  y0 = y1;
+  y1 = -1;
+  window_rect_fill(
+    x0, y0, x1, y1,
+    0.870588235f, 0.843137255f, 0.596078431f, 1
+  );
   // bird_animate
   bird_animate_timer += window_deltatime;
-  f32 bird_animate_progress = math_clamp(bird_animate_timer / bird_animate_duration, 0, 1);
+  const f32 bird_animate_duration = 1.f;
+  const f32 bird_animate_progress = math_min(bird_animate_timer / bird_animate_duration, 1);
+  const f32 bird_animate_offset = .04f;
+  const f32 bird_animate_y0 = .4f;
   switch (bird_animate_state) {
     case BIRD_ANIMATE_UP:
-      y0 = bird_y0 + bird_animate_offset * (1.f - bird_animate_progress);
+      y0 = bird_animate_y0 + bird_animate_offset * (1.f - bird_animate_progress);
       if (bird_animate_timer >= bird_animate_duration) {
         bird_animate_state = BIRD_ANIMATE_DOWN;
         bird_animate_timer = 0;
       }
       break;
     case BIRD_ANIMATE_DOWN:
-      y0 = bird_y0 + bird_animate_offset * bird_animate_progress;
+      y0 = bird_animate_y0 + bird_animate_offset * bird_animate_progress;
       if (bird_animate_timer >= bird_animate_duration) {
         bird_animate_state = BIRD_ANIMATE_UP;
         bird_animate_timer = 0;
@@ -93,7 +117,7 @@ void window_onrender() {
 
 i32 main(i32 argc, char** argv) {
   window_startup("Flappy Bird", "assets/flappy_bird_atlas.bin");
-  vertices_reserve(QUAD_VERTEX_COUNT * 4, QUAD_INDEX_COUNT * 4);
+  vertices_reserve(QUAD_VERTEX_COUNT * OBJECTS_COUNT, QUAD_INDEX_COUNT * OBJECTS_COUNT);
   window_run();
   return 0;
 }
